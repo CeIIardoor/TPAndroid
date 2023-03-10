07de747ad152a4fef452e94d7e08ca7ca6690ab4
CeIIardoor <laouinayassine@gmail.com>
2023-03-10 10:40:28 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/tpandroid/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tpandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView texte = findViewById(R.id.txt1);
+        texte.setText("Here we go");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity", "onStart() called");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+}
